b243e4eafe1a17393e82918ff5e648c7d310f5e1
Lauralyn Watson <lauralynw16@gmail.com>
2021-12-02 23:20:26 -0500

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+function addIngredients(item, ingredients) {
+  if (!ingredients.includes(item)) {
+    ingredients.push(item);
+  }
+}
+
+function formatPrice(dollarValue) {
+  return `$${dollarValue}`
+}
+
+function decreasePrice(price) {
+  return price - (price * 0.1)
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+return recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
